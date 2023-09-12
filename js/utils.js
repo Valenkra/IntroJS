cd197b043c88c85df72e7ad24adeb641e5056525
Valencha <47866545@est.ort.edu.ar>
2023-09-12 09:03:45 -0300

    v

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,13 @@
 function showCalcEdad(name){
     name = prompt("Como te llamas?");
+    if(name == ""){
+        name = "Sr/Sra Doe";
+    }
+    
     age = prompt("En que fecha naciste? Escribilo: AAAA-MM-DD !!");
+    while(age == NaN){
+        age = prompt("En que fecha naciste? Escribilo: AAAA-MM-DD !!");
+    }
     const resultado = document.getElementById("resultado");
     resultado.innerHTML = `Hola ${name}, tienes ${calcularEdad(age)} años!`;
 }
